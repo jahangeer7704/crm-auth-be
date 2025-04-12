8b679367bfe9972e7d1b057a146fd0e31f659a96
jahangeer7704 <jahangeer7704@gmail.com>
2025-04-12 20:01:09 +0530

    envValidator

--- infrastructure/server.ts
+++ infrastructure/server.ts
@@ -2,4 +2,8 @@ import { appLogger } from "@/utils/observability/logger/appLogger.js";
 import Express from "express";
 import { appConfig } from "@/config/readers/appConfig.js";
 
-
+class Server{
+    private static instance :Server;
+    
+  
+}
